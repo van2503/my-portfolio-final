ea4ce3acf16a2a8c967cf9c7e2f6a2ec601c1e93
Trịnh Thị Vân <trinhvan250305@gmail.com>
2025-11-17 23:19:46 +0700

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            document.title = "Portfolio | Vân Trịnh";
+            $("#favicon").attr("href", "assets/images/van2.jpg");
         }
         else {
             document.title = "Come Back To Portfolio";
--- projects/script.js
+++ projects/script.js
@@ -20,8 +20,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
-            $("#favicon").attr("href", "/assets/images/favicon.png");
+            document.title = "Projects | Portfolio  Vân Trịnh";
+            $("#favicon").attr("href", "/assets/images/van2.png");
         }
         else {
             document.title = "Come Back To Portfolio";
